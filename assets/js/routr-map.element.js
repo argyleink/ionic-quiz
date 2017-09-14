75d45450d670eef196fb9a381aa04cee823bdfd2
Adam Argyle <atom@argyleink.com>
2017-09-13 20:26:48 -0700

    shadow root map

--- assets/js/routr-map.element.js
+++ assets/js/routr-map.element.js
@@ -1,8 +1,20 @@
 export default class RoutrMap extends HTMLElement {
-  createdCallback() {}
+  createdCallback() {
+    this.shadow_root = this.attachShadow({mode: 'open'})
+    this.shadow_root.innerHTML = `
+      <style>
+        #map {
+          display: block;
+          width: 100vw;
+          height: 100vh;
+        }
+      </style>
+      <div id="map"></div>
+    `
+  }
 
   attachedCallback() {
-    this.map = new google.maps.Map(this, {
+    this.map = new google.maps.Map(this.shadow_root.querySelector('#map'), {
       zoom: 8,
       center: {
         lat: -34.397, 
