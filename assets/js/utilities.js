f0b4c679a7efa760d37395b4ad5ca3ac7fc9f267
Adam Argyle <atom@argyleink.com>
2017-09-14 21:19:22 -0700

    added headers the api suggests, added catch

--- assets/js/utilities.js
+++ assets/js/utilities.js
@@ -1,10 +1,16 @@
-const API_BASE = 'http://webservices.nextbus.com/service/publicXMLFeed?'
+const API_BASE      = 'http://webservices.nextbus.com/service/publicXMLFeed?'
+const API_HEADERS   = new Headers({'Accept-Encoding':'gzip, deflate'})
 
 const fetchXML = async (params) => {
-  let response  = await fetch(`${API_BASE}${params}`)
-  let text      = await response.text()
-  let xml       = await (new window.DOMParser()).parseFromString(text, 'text/xml')
-  return xml
+  try {
+    let response  = await fetch(`${API_BASE}${params}`, {headers: API_HEADERS})
+    let text      = await response.text()
+    let xml       = await (new window.DOMParser()).parseFromString(text, 'text/xml')
+    return xml
+  } 
+  catch(e) {
+    console.error(e)
+  }
 }
 
 export {
